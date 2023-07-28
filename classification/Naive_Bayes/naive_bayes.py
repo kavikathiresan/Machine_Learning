305a81d320e34dacdb52ccb3da594ad201f0e79e
Kavi <132807842+kavikathiresan@users.noreply.github.com>
2023-07-28 19:26:16 +0530

    Add files via upload

---
+++ classification/Naive_Bayes/naive_bayes.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import classification_report,confusion_matrix
+
+class NAIVE:
+    def __init__(self,path):
+        self.path=path
+        self.nb=pd.read_csv(self.path)
+        self.gau=GaussianNB()
+
+    def split_data(self):
+        try:
+            # convert categorical to numerical
+            self.nb['diagnosis'].unique()
+            self.nb['diagnosis'] = self.nb['diagnosis'].map({'M': 1, 'B': 0}).astype(int)
+            x = self.nb.iloc[:, 1:]
+            y = self.nb.iloc[:, 0]
+            x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+            return x_train, x_test, y_train, y_test
+        except Exception as e:
+            print(f'error in main:{e.__str__()}')
+
+    def modeling_data(self,x_train, x_test, y_train, y_test):
+        try:
+            self.gau.fit(x_train, y_train)
+            y_train_predict = self.gau.predict(x_train)
+            y_test_predict = self.gau.predict(x_test)
+            print(f'the Training accuracy:{self.gau.score(x_train, y_train)}')
+            print(f'the Testing accuracy:{self.gau.score(x_test, y_test)}')
+            # Validation Report
+            print(f'the Training confusion:{confusion_matrix(y_train,y_train_predict)}')
+            print(f'the Testing confusion:{confusion_matrix(y_test, y_test_predict)}')
+            print(f'the Training classification report:{classification_report(y_train, y_train_predict)}')
+            print(f'the Testing classification report:{classification_report(y_test, y_test_predict)}')
+
+        except Exception as e:
+            print(f'error in main:{e.__str__()}')
+
+
+    def dataprocessing(self):
+        try:
+            self.nb.isnull().sum()# checking null values
+            print(f'Missing null values:{self.nb.isna().sum()}')
+            self.nb=self.nb.drop(['Unnamed: 32','id'],axis=1)
+            print(f'OBESERVATION AND FEATURE:{self.nb.shape[0]} and {self.nb.shape[1]}')
+            x_train, x_test, y_train, y_test=self.split_data()
+            self.modeling_data(x_train, x_test, y_train, y_test)
+
+
+        except Exception as e:
+            print(f'error in main:{e.__str__()}')
+
+
+
+if __name__=='__main__':
+    obj=NAIVE("T:/pycharm/ML/supervisied_model/breast-cancer.csv")# load the dataset
+    obj.dataprocessing()
